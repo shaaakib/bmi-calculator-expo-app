b0258e140eb158c02c7def8c49749d7e6fc689d4
Muhammad Shakib Hossain <>
2023-06-21 08:16:56 +0600

    added main top title & desc

--- app/screens/BmiCalculator.jsx
+++ app/screens/BmiCalculator.jsx
@@ -1,9 +1,11 @@
-import { Image, Text, View } from 'react-native';
+import { Image, View } from 'react-native';
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { colors } from '../theme/colors';
 import { LinearGradient } from 'expo-linear-gradient';
 import { images } from '../theme/images';
+import { metrics } from '../theme/metrics';
+import Text from '../components/Text/Text';
 
 export default function BmiCalculator() {
   return (
@@ -13,10 +15,28 @@ export default function BmiCalculator() {
       end={[1, 1]}
       style={styles.container}
     >
+      {/* Top title and desc */}
       <View>
-        <View style={styles.logo}>
-          <Image source={images.icons} />
+        <View>
+          <View style={styles.logo}>
+            <Image source={images.icons} />
+          </View>
+          <View style={styles.MainTitle}>
+            <Text customStyles={{ textAlign: 'center' }} preset="headingXXL">
+              Body Mass Index Calculator
+            </Text>
+          </View>
+          <View style={styles.MainTitle}>
+            <Text customStyles={{ textAlign: 'center' }} preset="body">
+              Better understand your weight in relation to your height using our
+              body mass index (BM) calculator. While BMI is not the sole
+              determinant of a healthy weight, it offers a valuable starting
+              point to evaluate your overall health and well-being.
+            </Text>
+          </View>
         </View>
+
+        {/* bmi calculation */}
       </View>
     </LinearGradient>
   );
@@ -29,5 +49,10 @@ const styles = StyleSheet.create({
   logo: {
     resizeMode: 'contain',
     alignSelf: 'center',
+    marginTop: metrics.spacing.xl,
+  },
+  MainTitle: {
+    marginTop: metrics.spacing.l,
+    marginHorizontal: metrics.spacing.m,
   },
 });
